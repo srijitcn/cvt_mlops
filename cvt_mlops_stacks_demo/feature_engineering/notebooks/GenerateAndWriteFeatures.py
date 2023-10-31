80b5d44db51c7b791cb82429ccc1dbd8bb5991a1
srijitcn <srijitcn>
2023-10-31 16:27:45 +0000

    changes to feature creation

--- cvt_mlops_stacks_demo/feature_engineering/notebooks/GenerateAndWriteFeatures.py
+++ cvt_mlops_stacks_demo/feature_engineering/notebooks/GenerateAndWriteFeatures.py
@@ -8,7 +8,8 @@
 #
 # Parameters:
 #
-# * input_table_path (required)   - Path to input data.
+# * env (required)                - The deploy environment
+# * user_uuid (required)          - The deploy environment
 # * output_table_name (required)  - Fully qualified schema + Delta table name for the feature table where the features
 # *                                 will be written to. Note that this will create the Feature table if it does not
 # *                                 exist.
@@ -26,16 +27,18 @@
 # List of input args needed to run this notebook as a job.
 # Provide them via DB widgets or notebook arguments.
 #
-# A Hive-registered Delta table containing the input data.
-dbutils.widgets.text(
-    "input_table_path",
-    "/databricks-datasets/nyctaxi-with-zipcodes/subsampled",
-    label="Input Table Name",
-)
+# env
+dbutils.widgets.text("env","dev",label="Target Env")
+
+# user_uuid
+dbutils.widgets.text("user_uuid","8243",label="User UUID")
+
 # Input start date.
 dbutils.widgets.text("input_start_date", "", label="Input Start Date")
+
 # Input end date.
 dbutils.widgets.text("input_end_date", "", label="Input End Date")
+
 # Timestamp column. Will be used to filter input start/end dates.
 # This column is also used as a timestamp key of the feature table.
 dbutils.widgets.text(
@@ -45,18 +48,18 @@ dbutils.widgets.text(
 # Feature table to store the computed features.
 dbutils.widgets.text(
     "output_table_name",
-    "dev.cvt.trip_pickup_features",
+    "mlops_demo.cvt.patch_features",
     label="Output Feature Table Name",
 )
 
 # Feature transform module name.
 dbutils.widgets.text(
-    "features_transform_module", "pickup_features", label="Features transform file."
+    "features_transform_module", "patch_feature_extraction", label="Features transform file."
 )
 # Primary Keys columns for the feature table;
 dbutils.widgets.text(
     "primary_keys",
-    "zip",
+    "id",
     label="Primary keys columns for the feature table, comma separated.",
 )
 
@@ -69,9 +72,11 @@ notebook_path =  '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.ge
 
 
 # COMMAND ----------
-# DBTITLE 1,Define input and output variables
 
-input_table_path = dbutils.widgets.get("input_table_path")
+# DBTITLE 1,Define input and output variables
+env = dbutils.widgets.get("env")
+user_uuid = dbutils.widgets.get("user_uuid")
+
 output_table_name = dbutils.widgets.get("output_table_name")
 input_start_date = dbutils.widgets.get("input_start_date")
 input_end_date = dbutils.widgets.get("input_end_date")
@@ -79,26 +84,21 @@ ts_column = dbutils.widgets.get("timestamp_column")
 features_module = dbutils.widgets.get("features_transform_module")
 pk_columns = dbutils.widgets.get("primary_keys")
 
-assert input_table_path != "", "input_table_path notebook parameter must be specified"
 assert output_table_name != "", "output_table_name notebook parameter must be specified"
 
 # Extract database name. Needs to be updated for Unity Catalog.
-output_database = output_table_name.split(".")[0]
+output_catalog = output_table_name.split(".")[0]
+output_database = output_table_name.split(".")[1]
+
 
 # COMMAND ----------
+
 # DBTITLE 1,Create database.
-
-spark.sql("CREATE DATABASE IF NOT EXISTS " + output_database)
-
-# COMMAND ----------
-# DBTITLE 1, Read input data.
-
-raw_data = spark.read.format("delta").load(input_table_path)
-
+spark.sql(f"CREATE DATABASE IF NOT EXISTS {output_catalog}.{output_database}")
 
 # COMMAND ----------
+
 # DBTITLE 1,Compute features.
-
 # Compute the features. This is done by dynamically loading the features module.
 from importlib import import_module
 
@@ -106,15 +106,13 @@ mod = import_module(features_module)
 compute_features_fn = getattr(mod, "compute_features_fn")
 
 features_df = compute_features_fn(
-    input_df=raw_data,
-    timestamp_column=ts_column,
-    start_date=input_start_date,
-    end_date=input_end_date,
+    env = env,
+    user_uuid = user_uuid
 )
 
 # COMMAND ----------
-# DBTITLE 1, Write computed features.
 
+# DBTITLE 1, Write computed features.
 from databricks import feature_store
 
 fs = feature_store.FeatureStoreClient()
@@ -137,3 +135,7 @@ fs.write_table(
 )
 
 dbutils.notebook.exit(0)
+
+# COMMAND ----------
+
+
